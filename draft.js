1035bc9219481390304931a2dcf68a1be2d340e9
Huyen Duong <huyeduon@cisco.com>
2025-07-09 23:00:03 +0800

    duplicate id validation

--- draft.js
+++ draft.js
@@ -1,46 +1,5 @@
-const Tour = require('./models/tourModel');
-
-const catchAsync = (fn) => {
+module.exports = (fn) => {
   return (req, res, next) => {
     fn(req, res, next).catch((err) => next(err));
   };
 };
-
-exports.createTour = catchAsync(async (req, res, next) => {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-});
-
-// catchAsync is higher order function, it take a function fn and return a promise
-
-const fn = async (req, res, next) => {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-};
-
-const createTour = catchAsync(fn);
-
-createTour = catchAsync(async (req, res, next) => {
-  const newTour = await Tour.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
-});
-
-// route definition
-
-router.post('/tours', createTour);
-const createTour = catchAsync(fn);
