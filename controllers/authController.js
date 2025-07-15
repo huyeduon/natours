46ff144d25121f6b88bc7ba28a889322b3357683
Huyen Duong <huyeduon@cisco.com>
2025-07-15 23:36:39 +0800

    login user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,54 @@
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
 
+const AppError = require('./../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // create a jwt token that sign from userid
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  // 1) check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!'), 400);
+  }
+
+  // 2) check if user exists and password is correct
+  // password is not shown when get user (by default since we set select:false in user model).
+  // here we need to get password in order to authenticate user
+  const user = await User.findOne({ email: email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password'), 401);
+  }
+  console.log(user);
+  // 3) If everything is ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
