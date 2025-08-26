dc36c5407159da8fe8a7673263d475ee0598ad49
Huyen Duong <huyeduon@cisco.com>
2025-08-26 23:40:44 +0800

    add reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,34 @@
+const Review = require('./../models/reviewModel');
+
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+  if (!reviews || reviews.length == 0) {
+    return next(new AppError('No reviews yet'), 404);
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+const createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+module.exports = {
+  getAllReviews,
+  createReview,
+};
